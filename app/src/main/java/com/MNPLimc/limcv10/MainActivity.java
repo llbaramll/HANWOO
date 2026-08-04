6bb8eaa8968fa450b97cb85455cebd289369a3ce
llbaramll <llbaramll@gmail.com>
2026-08-04 17:54:31 +0900

    Update MainActivity.java

--- app/src/main/java/com/MNPLimc/limcv10/MainActivity.java
+++ app/src/main/java/com/MNPLimc/limcv10/MainActivity.java
@@ -1,4 +1,4 @@
-package com.MobileLimc.limcv10;
+package com.MNPLimc.limcv10;
 
 import android.os.Bundle;
 import android.webkit.WebSettings;
@@ -24,20 +24,20 @@ public class MainActivity extends AppCompatActivity {
 
         webView.setWebViewClient(new WebViewClient());
 
-        // Target SDK 36 이상 규격에 맞춘 최신 뒤로가기(Back Button) 처리
+        // Target SDK 36 지원 최신 뒤로가기(Back Button) 로직
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
                 if (webView != null && webView.canGoBack()) {
-                    webView.goBack(); // 웹뷰 내에 이전 페이지가 있으면 이전 페이지로 이동
+                    webView.goBack();
                 } else {
-                    setEnabled(false); // 더 이상 뒤로 갈 페이지가 없으면 백버튼 처리 해제 후 앱 종료
+                    setEnabled(false);
                     getOnBackPressedDispatcher().onBackPressed();
                 }
             }
         });
 
-        // 접속할 주소를 입력해 주세요 (필요시 수정)
+        // 웹뷰 연결 주소
         webView.loadUrl("https://www.naver.com");
     }
 }
